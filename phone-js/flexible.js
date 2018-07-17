caa1571ada05fccbcfb8ada8bd5b7bb3f696c53d
webxiaoma <webxiaoma@qq.com>
2018-07-17 12:56:43 +0800

    phone

--- phone-js/flexible.js
+++ phone-js/flexible.js
@@ -1,6 +1,6 @@
 function initPhone() {
 	var width = window.screen.width;
-	var device = window.devicePixelRatio || 2;
+	var device = window.devicePixelRatio || 1;
 	var scale = 1 / device;
 	var meta = document.createElement("meta");
 	meta.setAttribute("name", "viewport");
@@ -21,10 +21,22 @@ window.onresize = function() {
  
 - 主要思路：
 
-1. 通过 `window.devicePixelRatio` 来获取手机的设备像素比，默认为2
+1. 通过 `window.devicePixelRatio` 来获取手机的设备像素比，默认为1
 2. 将页面按照像素比进行缩小，
 3. 这时，页面上所有的东西都缩小了，我们需要做的时把页面要放大，并实现自适应,我们通过rem来实现页面的自适应。比如我们的设计图宽度时750px, 1rem = 100px , 这里我们应该又这样的公式
   
    根节点的字体大小  =  手机实际宽度 X 设备像素比 / 7.5
 
  */
+// function initPhone() {
+// 	var width = window.screen.width;
+// 	var device = window.devicePixelRatio || 1;
+// 	var content = "width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1";
+// 	document.documentElement.setAttribute("data-dpr", device);
+// 	document.documentElement.style.fontSize = width  / 7.5 + "px"
+// }
+// initPhone();
+// window.onresize = function() {
+// 	initPhone()
+// };
+
